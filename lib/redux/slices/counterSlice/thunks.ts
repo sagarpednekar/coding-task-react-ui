8e2bc2dba1f09b0e4ee348a03bc0c550323f3343
Sagar Pednekar <sagar.pednekar@accionlabs.com>
2023-12-04 22:11:13 +0530

    feat(counter): add reducer for managing counter state

--- lib/redux/slices/counterSlice/thunks.ts
+++ lib/redux/slices/counterSlice/thunks.ts
@@ -1,8 +1,15 @@
 /* Instruments */
-import type { ReduxThunkAction } from "@/lib/redux";
+import { ReduxState, ReduxThunkAction, incrementByAmount } from "@/lib/redux";
 
 export const incrementIfOddAsync =
   (amount: number): ReduxThunkAction =>
-  () => {
+  (dispatch, getState: () => ReduxState) => {
     // update only if currentValue is odd
+    const {
+      counter: { value },
+    } = getState();
+    if (value % 2 === 1) dispatch(incrementByAmount(amount));
+    else {
+      throw new Error("Current counter value is not a odd number")
+    }
   };
